4497de95141b3148256207b05aef29d314f90b95
chontezefranklin <115087283+chontezefranklin@users.noreply.github.com>
2023-04-20 22:21:03 -0500

    Create hw4lab.py

---
+++ hw4/hw4lab.py
@@ -0,0 +1,40 @@
+import socket
+import pandas as pd
+import graphviz
+
+# Read domains from CSV file
+domains = pd.read_csv("domains.tsv", sep='\t')['domain']
+
+# Initialize variables
+main_dom = 1
+dns = {}
+last = {}
+addr = {}
+node = 300
+
+# Initialize graph
+dot = graphviz.Digraph(engine='fdp', comment='digraphtesting')
+
+# Loop through domains
+for dom in domains:
+    try:
+        # Perform forward DNS lookup
+        address = socket.getaddrinfo(dom, 443, type=socket.SOCK_STREAM)
+
+        # Add node for domain to graph
+        main_dom += 1
+        with dot.subgraph() as s:
+            s.node(str(main_dom), dom)
+
+        # Loop through IP addresses and perform reverse DNS lookup
+        for j in range(len(address)-1):
+            try:
+                port = address[j][4]
+                rev_dns = socket.gethostbyaddr(port[0])[0]
+                node += 1
+                dot.node(str(node), rev_dns)
+                dot.edge(str(node), str(main_dom))
+            except:
+                pass
+    except:
+        pass
